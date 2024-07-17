8356515911c01f640f1c0904622480e9206390bc
Carmelo Seminara <89209493+carme-10@users.noreply.github.com>
2024-07-17 11:55:04 +0200

    Add pipeline

---
+++ pipeline/python/bin/address_info_collector.py
@@ -0,0 +1,408 @@
+import requests
+import json
+
+from fuzzywuzzy import fuzz, process
+
+import time, copy
+import os
+
+#nel file addresses.txt bisogna inserire gli indirizzi da cercare.
+#ogni indirizzo deve essere scritto su una riga
+#nella forma:
+    #Via X, altro...
+    #cioè l'importante è che il nome sia scritto per prima e che sia seguita da una virgola,
+    #la quale deve essere per la prima volta presente in quel momento
+
+#get api key from file
+with open('../keys/TomTom_key.txt', 'r') as file:
+    tom_tom_api_key = file.read().strip()
+
+with open('../keys/OpenWeather_key.txt', 'r') as file:
+    open_weather_api_key = file.read().strip()
+
+#function to read input from a json file and call the api
+def get_addresses_info(file_name):
+
+    with open(file_name, 'r') as file:
+        addresses = file.readlines()
+
+    output = []
+    
+    for address in addresses:
+
+        result = api_calls(address.strip())
+
+        if result is None:
+            print(f"Error, address: '{address}' info not added")
+            continue
+        else:
+
+            if len(result['elements']) == 0:
+                print('No elements found', address)
+                continue
+
+            index = address.index(',')
+            input_way_name = address[:index]
+
+            result_filtered = json_filter(result, input_way_name)
+
+            if result_filtered is None:
+                print('No elements is matching the input', input_way_name, result)
+                continue
+
+            output.extend(result_filtered)
+
+            print('Request completed successfully', address)
+
+    return output
+
+
+def api_calls(address):
+    #find the latitude and longitude of the address
+    tomtom_response = call_TomTom_api(address)
+
+    if tomtom_response is None:
+        return None
+    
+    lat = tomtom_response[0]
+    lon = tomtom_response[1]
+
+    query = f"""
+        [out:json];
+        way(around:600, {lat}, {lon})["name"]["highway"];
+        out body;
+        >;
+        out skel qt;
+    """
+    overpass_response = call_overpass_api(query)
+
+    if overpass_response is None:
+        return None
+    
+    return overpass_response
+
+def call_TomTom_api(address):
+    
+    url = f"https://api.tomtom.com/search/2/geocode/{address}.json"
+
+    params = {
+        'key': tom_tom_api_key
+    }
+
+    try:
+        
+        response = requests.get(url, params=params)
+
+        response.raise_for_status()  # Raises HTTPError, if one occurred.
+        api_response = response.json()  
+
+        # Access the first result's position directly
+        position = api_response['results'][0]['position']
+
+        lat = position['lat']
+        lon = position['lon']
+
+        return [lat, lon]
+    
+    except requests.exceptions.RequestException as e:
+        print(f'Error making TomTom API request: {e}')
+        return None
+    
+
+def call_overpass_api(query):
+   
+    url = 'https://overpass-api.de/api/interpreter'
+
+    try:
+        
+        response = requests.post(url, data=query)
+
+        response.raise_for_status()  
+        api_response = response.json()
+
+        return api_response 
+
+    except requests.exceptions.RequestException as e:
+        print(f'Error making Overpass API request: {e}')
+        return None
+
+
+def json_filter(input_json, way_name):
+        
+    # Filter elements of type 'way'
+    way_elements = [el for el in input_json['elements'] if el['type'] == 'way']
+        
+    # Compare tags->name with input string using fuzzywuzzy,
+    #I use a tuple as key of the dictionary, since the streets
+    #can be divided into multiple elements with the same "name" but different "id"
+    names = {(el['id'] ,el['tags']['name']): el for el in way_elements}
+    keys = names.keys()
+    
+    #transform the tuples into a dictionary that has as key the first element of the tuple and as value the second
+    id_name_dict = {tupla[0]: tupla[1] for tupla in keys}
+    
+    best_match = process.extract(way_name, id_name_dict, scorer=fuzz.token_sort_ratio, limit=100) #get a list with tuples (match, score, match key)
+
+    best_match_filtered = [el for el in best_match if el[1] >= 80]
+    
+    if best_match_filtered == []:
+        return None
+    
+    best_match_name = best_match_filtered[0][0]
+
+    way_ids = [el[2] for el in best_match_filtered if el[0] == best_match_name]
+
+    # Get the elements with the highest score (same name so same score)
+    best_elements = [names[(id, best_match_name)] for id in way_ids]
+
+    #get array nodes from best_elements
+    node_ids = [] #a list of lists...
+    for i in range(len(best_elements)):
+        node_ids.append(best_elements[i]["nodes"]) #we are adding a row, because we are adding a list
+
+    node_elements = [] #a list of lists...
+    for i in range(len(node_ids)):
+        node_elements.append( [el for el in input_json['elements'] if el['type'] == 'node' and el['id'] in node_ids[i]] )
+
+    output = []
+    for i in range(len(best_elements)):
+        combined_json = {
+            "way_info": best_elements[i],
+            "way_nodes_info": node_elements[i]
+        }
+        output.append(combined_json)
+
+    return output
+
+def filter_addresses_info(input):
+
+    output = []
+
+    for el in input:
+
+        way_info = el["way_info"]
+        data = {
+            "way_id": way_info["id"],
+            "highway": way_info["tags"]["highway"],
+            "name": way_info["tags"]["name"]
+        }
+        way_nodes_info = el["way_nodes_info"]
+
+        coppie = [(node["lat"], node["lon"]) for node in way_nodes_info]
+
+        data["lista_coppie"] = coppie
+
+        output.append(data)
+
+    return output
+
+
+def get_point_info(lat, lon):
+    
+    tags = ["highway=crossing", "highway=give_way", "railway", \
+            "highway=stop", "traffic_calming", "highway=traffic_signals", "amenity", "noexit", "junction"]
+    
+    #create a dict with the tags as key and integer(...boolean) as value
+    output_dict = {tag: 0 for tag in tags}
+
+    query = f"""
+        [out:json];
+        (
+
+        node(around:30, {lat}, {lon})[{tags[0]}];
+        node(around:30, {lat}, {lon})[{tags[1]}];
+        node(around:30, {lat}, {lon})[{tags[2]}];
+        node(around:30, {lat}, {lon})[{tags[3]}];
+        node(around:30, {lat}, {lon})[{tags[4]}];
+        node(around:30, {lat}, {lon})[{tags[5]}];
+        node(around:100, {lat}, {lon})[{tags[6]}];
+        node(around:100, {lat}, {lon})[{tags[7]}];
+        node(around:100, {lat}, {lon})[{tags[8]}];
+        );
+        out body;
+        >;
+        out skel qt;
+    """
+
+    overpass_response = call_overpass_api(query)
+
+    if overpass_response is None:
+        print("Error making Overpass API request for points of interest", lat, lon)
+        return None
+    else:
+
+        #get all the elements
+        elements = overpass_response['elements']
+        
+        #if there are no elements the cycle is not executed, but it is not a problem
+        for el in elements:          
+
+            tmp_tags = el['tags'] #for example {'traffic_calming': 'bump', 'highway': 'crossing'}
+
+            for key, value in tmp_tags.items():
+                k = key
+                v = value
+                k_v= k + "=" + v
+
+                if k_v in tags:
+                    output_dict[k_v] = 1
+                elif k in tags:
+                    output_dict[k] = 1
+
+        #I have to create a dictionary equal to output_dict but with different keys
+        #so I have to define a mapping
+        mapping= {
+            "highway=crossing": "Crossing", 
+            "highway=give_way": "Give_Way", 
+            "railway": "Railway", 
+            "highway=stop": "Stop",
+            "traffic_calming": "Traffic_Calming",
+            "highway=traffic_signals": "Traffic_Signal",
+            "amenity": "Amenity",
+            "noexit": "No_Exit",
+            "junction": "Junction"
+        }
+
+        output_dict = {mapping[k]: v for k, v in output_dict.items()}
+
+        output_dict["lat"] = lat
+        output_dict["lon"] = lon
+        
+        print("Output_dict of point " + str(lat) + " " + str(lon) + " is ", output_dict)
+        return output_dict
+
+
+def add_nodes_info(addresses_info):
+
+    for address in addresses_info:
+
+        data = {
+            'nodes_info': []
+        }
+
+        for index, coppia in enumerate(address["lista_coppie"]):
+
+            result = get_point_info(coppia[0], coppia[1])
+
+            #add an element to the list nodes_info
+            data['nodes_info'].append(result)
+
+            time.sleep(1)
+
+            if result is None:
+                exit(1)
+
+        address.update(data)
+
+#remember that dictionaries are passed by reference
+def insert_weather(input):
+
+    units = "metric"
+
+    output = copy.deepcopy(input)
+
+    for el in output:
+        
+        #we take the first node of the road that will be the reference for the weather
+        lat = el["lista_coppie"][0][0]
+        lon = el["lista_coppie"][0][1]
+
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units={units}&appid={open_weather_api_key}"
+
+        try:
+             
+            response = requests.get(url)
+
+            response.raise_for_status()  #Raises HTTPError, if one occurred
+
+            data = response.json()
+
+            if 'weather' in data and data['weather']:
+                data = {"weather_info": data}
+                el.update(data)
+            else:
+                print("No weather field found for the specified coordinates")
+                return None
+        
+        except requests.exceptions.RequestException as e:
+            print(f'Error making OpenWeather API request: {e}', lat, lon)
+            return None
+        
+    return output
+
+
+def check_logstash():
+
+    #try to make a request until logstash is up
+    while True:
+        time.sleep(5)
+        try:
+        
+            response = requests.get("http://logstash:9090")
+            response.raise_for_status() 
+            break
+
+        except requests.exceptions.RequestException as e:
+            print(f'Waiting for Logstash: {e}')
+
+def send_to_logstash(json_string):
+
+    try:
+        
+        response = requests.post("http://logstash:9090", data=json_string, \
+                                        headers={"Content-Type": "application/json"})
+        response.raise_for_status() 
+
+    except requests.exceptions.RequestException as e:
+        print(f'Error making request to Logstash: {e}')
+
+
+if __name__ == '__main__':
+
+    execution_mode = os.getenv('MY_MODE_VAR', 'Default') 
+
+    if execution_mode == 'Demo':
+        print('Demo mode')
+
+        check_logstash()
+        print('Logstash is up')
+
+        with open('../input_data/demo_mode/demo_input.jsonl', 'r') as file:
+            #to continue sending the data (even if the same)
+            while True:
+                for line in file:
+                    time.sleep(5)
+                    send_to_logstash(line)
+                
+                file.seek(0) #to read the file from the beginning
+
+    elif execution_mode == 'Default':
+        print('Default mode')
+
+        addresses_info = get_addresses_info("../input_data/addresses.txt")
+        addresses_info = filter_addresses_info(addresses_info)
+
+        add_nodes_info(addresses_info)
+        #print(addresses_info)
+
+        check_logstash()
+        print('Logstash is up')
+
+        while True:
+
+            output = insert_weather(addresses_info)
+            if output is None:
+                time.sleep(60)
+                continue
+
+            for el in output:
+
+                json_string = json.dumps(el)
+
+                send_to_logstash(json_string)
+            
+            time.sleep(30)
+        
+    else:
+        print('Unknown mode')
+        exit(1)
---
+++ pipeline/spark/code/from_kafka_to_es.py
@@ -0,0 +1,197 @@
+from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import from_json
+import pyspark.sql.types as tp
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf, substring, when, struct
+from pyspark.ml.pipeline import PipelineModel
+from pyspark.conf import SparkConf
+
+import requests, json, time
+
+def es_mapping(es_index):
+
+    #URL of the Elasticsearch index
+    url = f"http://elasticsearch:9200/{es_index}"
+
+    #mapping data in JSON format
+    mapping = {
+        "mappings": {
+            "properties": {
+                "weather_timestamp_iso8601": {
+                    "type": "date",
+                    "format": "strict_date_optional_time||epoch_millis"
+                },
+                "location": { 
+                    "type": "geo_point"
+                }
+            }
+        }
+    }
+
+    #try to make a request until es is available
+    while True:
+        time.sleep(1)
+        try:
+            #Send a PUT request to configure the mapping of the index
+            response = requests.put(url, headers={"Content-Type": "application/json"}, data=json.dumps(mapping))
+            response.raise_for_status()
+            
+            print("Elastic Search, mapping response:", response.text)
+            break
+
+        except requests.exceptions.RequestException as e:
+            print(f'Waiting for Elastic Search: {e}')
+
+def is_mapping_present(es_index):
+    
+    url = f"http://elasticsearch:9200/{es_index}/_mapping"
+
+    while True:
+        time.sleep(1)
+        try:
+            # Send a GET request to get the mapping of the index
+            response = requests.get(url)
+            response.raise_for_status()
+
+            mapping_data = response.json()
+            if mapping_data:
+                print(f"Mapping for index '{es_index}' found")
+                return True
+            else:
+                print(f"No mapping found for index '{es_index}'")
+                return False
+
+        except requests.exceptions.RequestException as e:
+            #In case the index does not exist, the response will be 404
+            if e.response.status_code == 404:
+                print(f"The index '{es_index}' does not exist")
+                return False
+            else:
+                print(f'Waiting for Elastic Search: {e}')
+
+
+kafkaServer="broker:9092"
+topic = "roads-topic"
+
+elastic_index="road_data"
+
+if not is_mapping_present(elastic_index):
+    es_mapping(elastic_index)
+
+print("Starting the Application")
+
+sparkConf = SparkConf().set("es.nodes", "elasticsearch") \
+                        .set("es.port", "9200")
+
+spark = SparkSession.builder.appName("ReadFromKafkaAndWriteToES").config(conf=sparkConf).getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR") 
+
+print("Loading model")
+model = PipelineModel.load("/opt/tap/models/RandomForestRegression")
+print("Done")
+
+#user defined function to map the weather icon to a more readable string
+def map_icon(icon):
+    if icon == "01d" or icon == "01n":
+        return "clear sky"
+    if icon == "02d" or icon == "02n":
+        return "few clouds"
+    if icon == "03d" or icon == "03n":
+        return "scattered clouds"
+    if icon == "04d" or icon == "04n":
+        return "broken clouds"
+    if icon == "09d" or icon == "09n":
+        return "shower rain"
+    if icon == "10d" or icon == "10n":
+        return "rain"
+    if icon == "11d" or icon == "11n":
+        return "thunderstorm"
+    if icon == "13d" or icon == "13n":
+        return "snow"
+    if icon == "50d" or icon == "50n":
+        return "mist"
+    return "unknown"
+
+columns_struct = tp.StructType([
+    tp.StructField(name= 'way_id', dataType= tp.LongType(),  nullable= True),
+    tp.StructField(name= 'highway', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'name',       dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= 'Temperature_C', dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= 'Humidity_%', dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= 'pressure_hPa', dataType= tp.DoubleType(),  nullable= True),
+    tp.StructField(name= 'wind_speed_meterSec', dataType= tp.DoubleType(),  nullable= True),
+    tp.StructField(name= 'weather_icon', dataType= tp.StringType(),  nullable= True),
+    tp.StructField(name= "Give_Way", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "Crossing", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "Traffic_Calming", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "Traffic_Signal", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "Railway", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "Stop", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "Amenity", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "No_Exit", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "Junction", dataType= tp.IntegerType(),  nullable= True),
+    tp.StructField(name= "lat", dataType= tp.DoubleType(),  nullable= True),
+    tp.StructField(name= "lon", dataType= tp.DoubleType(),  nullable= True),
+    tp.StructField(name= "weather_timestamp_iso8601", dataType= tp.StringType(),  nullable= True)
+])
+
+
+# Read the stream from kafka
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafkaServer) \
+    .option("subscribe", topic) \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+data_columns_str = ["data."+field.name for field in columns_struct.fields]
+
+# Cast the message received from kafka with the provided schema
+df = df.selectExpr("CAST(value AS STRING)") \
+    .select(from_json("value", columns_struct).alias("data")) \
+    .select(data_columns_str)
+
+#rename columns
+df= df.withColumnRenamed("Temperature_C", "Temperature(C)")
+df= df.withColumnRenamed("Humidity_%", "Humidity(%)")
+
+df= df.withColumn("Pressure(in)", (col("pressure_hPa")*0.02953).cast(tp.IntegerType()))
+df= df.withColumn("Wind_Speed(mph)", (col("wind_speed_meterSec")*2.23694).cast(tp.IntegerType()))
+
+#create a new column with the last char of the weather_icon as value (d or n),
+#spark string index starts from 1, that's why the 3
+df = df.withColumn("Sunrise_Sunset", substring(df["weather_icon"], 3, 1))
+#convert the d/n in Day/Night
+df = df.withColumn("Sunrise_Sunset", when(df["Sunrise_Sunset"] == "d", "Day").otherwise("Night"))
+
+map_icon_udf = udf(map_icon)
+df = df.withColumn("Weather_Condition", map_icon_udf(col("weather_icon")))
+
+df = df.drop("pressure_hPa", "wind_speed_meterSec", "weather_icon")
+
+# # Apply the machine learning model
+df=model.transform(df)
+
+#location column with lat and lon
+df = df.withColumn("location", struct(col("lat"), col("lon")))
+
+df = df.select("way_id", "highway", "name", "Temperature(C)", "Humidity(%)", "Pressure(in)", \
+               "Wind_Speed(mph)", "Sunrise_Sunset", "Weather_Condition", "Amenity", "Give_Way", \
+                   "Crossing", "Traffic_Calming", "Traffic_Signal", "Railway", "Stop", "No_Exit", \
+                    "Junction", "location", "weather_timestamp_iso8601", "prediction")
+
+df = df.withColumnRenamed("prediction", "danger_index")
+
+# df.writeStream \
+#    .format("console") \
+#    .option("truncate",True) \
+#    .start() \
+#    .awaitTermination()
+
+df.writeStream \
+   .option("checkpointLocation", "/tmp/") \
+   .format("es") \
+   .start(elastic_index) \
+   .awaitTermination()
